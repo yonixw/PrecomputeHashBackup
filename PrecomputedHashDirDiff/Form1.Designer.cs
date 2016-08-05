5553dd33e50f20bfe25b95c4c9848be54adcf585
yonixw2 <yo2@gmail.com>
2016-08-05 16:52:24 +0300

    Before compare algo

--- PrecomputedHashDirDiff/Form1.Designer.cs
+++ PrecomputedHashDirDiff/Form1.Designer.cs
@@ -34,6 +34,11 @@
             this.pbHashStatus = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCreateList
@@ -85,11 +90,57 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Compare premaid databases:";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 192);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(567, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Backup db (Source):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Live Folder db (Target):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 235);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(567, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(478, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Compare hash list";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(601, 436);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pbHashStatus);
@@ -111,6 +162,11 @@
         private System.Windows.Forms.ProgressBar pbHashStatus;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- PrecomputedHashDirDiff/PrecomputeHash.cs
+++ PrecomputedHashDirDiff/PrecomputeHash.cs
@@ -101,7 +101,7 @@ namespace PrecomputedHashDirDiff
 
         public static string byte2hum(long bytes)
         {
-            string result = "(TODO) ";
+            string result = "";
 
             long KB = 1024;
             long MB = 1024 * KB;
@@ -110,17 +110,49 @@ namespace PrecomputedHashDirDiff
 
             long units = 0;
 
+            // TERA
             while (bytes > TB) {
                 units++;
                 bytes = bytes - TB;
             }
-            
 
+            result += units + " TB, ";
+            units = 0;
+
+            // GIGA
+            while (bytes > GB)
+            {
+                units++;
+                bytes = bytes - GB;
+            }
+
+            result += units + " GB, ";
+            units = 0;
+
+            // Mega
+            while (bytes > MB)
+            {
+                units++;
+                bytes = bytes - MB;
+            }
+
+            result += units + " MB, ";
+            units = 0;
+
+            // Kilo
+            while (bytes > KB)
+            {
+                units++;
+                bytes = bytes - KB;
+            }
+
+            result += units + " KB, ";
+            units = 0;
+
+            result += bytes + " Bytes.";
             return result;
 
-
-
-
+            
         }
     }
 }
