4347a49b73c3dcae1baf8f2cb544e2efcf33b265
yonixw2 <yo2@gmail.com>
2016-10-30 01:28:26 +0300

    Added server json structure and functions... now to insert those into logic

--- PrecomputeBackupManager/BackupFlow/2UnlockUpload.cs
+++ PrecomputeBackupManager/BackupFlow/2UnlockUpload.cs
@@ -7,6 +7,8 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,15 +19,128 @@ namespace PrecomputeBackupManager
 {
     public partial class Form1 : Form
     {
+        #region ServerStatus
+
+        [DataContract]
+        public class BackupProcessStatus
+        {
+            // Integers:
+
+            [DataMember]
+            public int id;
+            
+            [DataMember]
+            public int addedbytes;
+
+            [DataMember]
+            public int statuscode;
+
+            // Strings:
+
+            [DataMember]
+            public string username;
+            
+            [DataMember]
+            public string start;
+            
+            [DataMember]
+            public string end;
+            
+            [DataMember]
+            public string statusdescription;
+        }
+
+        private static string ToJson<T>(T data)
+        {
+            // SO ? 1178255
+
+            DataContractJsonSerializer serializer
+                        = new DataContractJsonSerializer(typeof(T));
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, data);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        private static T FromJson<T>(string json)
+        {
+            // https://blog.udemy.com/json-serializer-c-sharp/
+
+            DataContractJsonSerializer serializer
+                        = new DataContractJsonSerializer(typeof(T));
+
+            using (MemoryStream ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            {
+                return (T)serializer.ReadObject(ms);
+            }
+        }
+
+        private BackupProcessStatus getBackupProcessUpdate(int updateId)
+        {
+            string pathToDownload = txtBackupApiURL + "/status.php?id=" + updateId.ToString();
+            pathToDownload = pathToDownload.Replace(@"//", @"/");
+
+            string result = (new WebClient()).DownloadString(pathToDownload);
+
+            Log("Status downloaded from server:\n" + JsonPrettyPrinterPlus.PrettyPrinterExtensions.PrettyPrintJson(result));
+
+            return FromJson<BackupProcessStatus>(result);
+        }
+
+        // Json for locking\unlocking 
+        [DataContract]
+        public class BackupActionResult
+        {
+            [DataMember]
+            public int updateid;
+            
+            [DataMember]
+            public string jobstatus;
+            
+        }
+
+        private BackupActionResult serverunlock(int userid)
+        {
+            string pathToDownload = txtBackupApiURL + "/unlock.php?id=" + userid.ToString();
+            pathToDownload = pathToDownload.Replace(@"//", @"/");
+
+            string result = (new WebClient()).DownloadString(pathToDownload);
+
+            Log("Unlock server result:\n" + JsonPrettyPrinterPlus.PrettyPrinterExtensions.PrettyPrintJson(result));
+
+            return FromJson<BackupActionResult>(result);
+        }
+
+        private BackupActionResult serverlock(int userid, int updateId, int backupsize = 0) 
+        {
+            string pathToDownload = txtBackupApiURL + "/lock.php?userid=" + userid.ToString() + "&id=" + updateId + "&bytes=" + backupsize;
+            pathToDownload = pathToDownload.Replace(@"//", @"/");
+
+            string result = (new WebClient()).DownloadString(pathToDownload);
+
+            Log("Lock server result:\n" + JsonPrettyPrinterPlus.PrettyPrinterExtensions.PrettyPrintJson(result));
+
+            return FromJson<BackupActionResult>(result);
+        }
+
+        #endregion
+
         #region Backup Step 2 - Unlock
+
+
+
 
         private void backworkUnlock_DoWork(object sender, DoWorkEventArgs e)
         {
             //Unlock using remote url
             Log("Step 2/4: Asking server to unlock.");
+            
 
             // Wait until we get a response that 
             // Of course, stop if we got a cancel and try to "cancel" (url should support 3 types: unlock, lock, cancel)
+
         }
 
         private void backworkUnlock_ProgressChanged(object sender, ProgressChangedEventArgs e)
