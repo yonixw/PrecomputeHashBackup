6d4660aed07d96bdf1215da081845d3ecc8acf9e
yonixw2 <yo2@gmail.com>
2016-12-01 20:51:15 +0200

    Added Decision form

---
+++ PrecomputeBackupManager/frmBackupErrorDecision.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PrecomputeBackupManager
+{
+    partial class frmBackupErrorDecision
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnTryAgain = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "An error occured while trying to upload a file:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(516, 142);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnTryAgain
+            // 
+            this.btnTryAgain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnTryAgain.Location = new System.Drawing.Point(17, 180);
+            this.btnTryAgain.Name = "btnTryAgain";
+            this.btnTryAgain.Size = new System.Drawing.Size(99, 34);
+            this.btnTryAgain.TabIndex = 2;
+            this.btnTryAgain.Text = "Try Again";
+            this.btnTryAgain.UseVisualStyleBackColor = false;
+            this.btnTryAgain.Click += new System.EventHandler(this.btnTryAgain_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancel.Location = new System.Drawing.Point(403, 180);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 34);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel Backup";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmBackupErrorDecision
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 222);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnTryAgain);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmBackupErrorDecision";
+            this.Text = "Please Decide";
+            this.Load += new System.EventHandler(this.frmBackupErrorDecision_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnTryAgain;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ PrecomputeBackupManager/frmBackupErrorDecision.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrecomputeBackupManager
+{
+    public partial class frmBackupErrorDecision : Form
+    {
+        string _filename;
+        Exception _ex;
+
+        public frmBackupErrorDecision(string fileName, Exception ex)
+        {
+            InitializeComponent();
+            _filename = fileName;
+            _ex = ex;
+        }
+
+        private void frmBackupErrorDecision_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTryAgain_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
