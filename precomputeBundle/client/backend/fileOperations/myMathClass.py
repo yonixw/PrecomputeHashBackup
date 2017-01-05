0d08ef1c9b2792f29bab1e63608131c3ebccd688
yonixw <yonixw@gmail.com>
2017-01-06 00:25:56 +0200

    in PyCharm must use full package path to work with all tests also read SO? 38569992 for unit test file bugs

---
+++ precomputeBundle/client/backend/fileOperations/myMathClass.py
@@ -0,0 +1,12 @@
+class myMathClass:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
